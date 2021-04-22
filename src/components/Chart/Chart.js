8f2f5fff5e6590cdceb117f4c9bc3e976c57a518
shhb0420 <shohib.yoona@gmail.com>
2021-04-22 18:41:40 +0700

    add line chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,18 +3,17 @@ import "./Chart.css";
 import { DateRange } from "react-date-range";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
-import { Doughnut, Pie, Line, Bar } from "react-chartjs-2";
+import { Polar, Pie, Line, Bar } from "react-chartjs-2";
 import { API_URL } from "../../utils/environment";
 import axios from "axios";
-import { NavItem } from "react-bootstrap";
-import parseISOWithOptions from "date-fns/fp/parseISOWithOptions/index";
 
 const Chart = () => {
-  const [order, setOrder] = useState();
+  const [order, setOrder] = useState([]);
   const [item, setItem] = useState([]);
   const [revenue, setRevenue] = useState();
   const [user, setUser] = useState([]);
   const [status, setStatus] = useState([]);
+  const [day, setDay] = useState([]);
 
   const handleSelect = () => {
     alert("hiya");
@@ -62,13 +61,34 @@ const Chart = () => {
 
         // console.log(
         //   Object.values(items)
-        //     .map((arr) => arr.reduce((sum, item) => (sum += item), 0))
-        //     .toLocaleString("en-US", {
-        //       style: "currency",
-        //       currency: "USD",
-        //     })
+        // .map((arr) => arr.reduce((sum, item) => (sum += item), 0))
+        // .toLocaleString("en-US", {
+        //   style: "currency",
+        //   currency: "USD",
+        // })
         // );
-        setOrder(data);
+
+        const date = data
+          .filter((item) => item.start_date)
+          .map((item) => new Date(item.start_date))
+          .toLocaleString("en-US", {
+            weekday: "short",
+          })
+          .split(",")
+          .reduce((list, day) => {
+            if (list[day]) {
+              list[day] = list[day] + 1;
+            } else {
+              list[day] = 1;
+            }
+            return list;
+          }, []);
+
+        const dataRevenue = data
+          .filter((item) => item.conversion_revenue)
+          .map((item) => Number(item.conversion_revenue));
+
+        setOrder(dataRevenue);
         setItem(Object.keys(items));
         setRevenue(
           Object.values(items).map((arr) =>
@@ -77,6 +97,7 @@ const Chart = () => {
         );
         setUser(Object.keys(users));
         setStatus(Object.values(users));
+        setDay(Object.keys(date));
       })
       .catch((err) => {
         console.log(err);
@@ -103,6 +124,33 @@ const Chart = () => {
         backgroundColor: ["#725E9C", "#E4EAEB", "#EBA45E", "#5C8F94"],
         hoverBackgroundColor: ["#725E9C", "#E4EAEB", "#EBA45E", "#5C8F94"],
         hoverOffset: 4,
+      },
+    ],
+  };
+
+  const dataForecastLine = {
+    labels: day,
+    datasets: [
+      {
+        label: "",
+        fill: "-2",
+        lineTension: 0.1,
+        backgroundColor: "#789764",
+        borderColor: "#789764",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "#789764",
+        pointBackgroundColor: "#ce6300",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "#ff7b00",
+        pointHoverBorderColor: "#ce6300",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: order,
       },
     ],
   };
@@ -140,7 +188,7 @@ const Chart = () => {
             <p>...</p>
           </div>
           <div style={{ height: 300, alignContent: "center", marginTop: 25 }}>
-            <Pie
+            <Polar
               width={350}
               height={150}
               options={{
@@ -159,6 +207,29 @@ const Chart = () => {
           <div className="d-flex justify-content-between">
             <p>Revenue</p>
             <p>...</p>
+          </div>
+          <div style={{ height: 300, alignContent: "center", marginTop: 25 }}>
+            <Line
+              width={50}
+              height={50}
+              options={{
+                maintainAspectRatio: false,
+                legend: {
+                  display: false,
+                  position: "bottom",
+                },
+              }}
+              data={dataForecastLine}
+            />
+            <p className="text-total">Total Revenue</p>
+            <p className="jumlah-revenue">
+              {order
+                .reduce((sum, item) => (sum += item), 0)
+                .toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })}
+            </p>
           </div>
         </div>
       </div>
