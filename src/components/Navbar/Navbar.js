eb20ca86f18aa02538ff6350ecd66f4c81ea13b1
shhb0420 <shohib.yoona@gmail.com>
2021-04-22 21:50:18 +0700

    Starting deploy

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Form } from "react-bootstrap";
 import Logo from "../../assets/icon/Logo.png";
